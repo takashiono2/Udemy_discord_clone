dbfdef6755cb3dcc9479897ece419b14255c0e36
takashiono2 <takashiono710@gmail.com>
2023-08-31 23:13:39 +0900

    section3_17

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import './Sidebar.scss';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import AddIcon from '@mui/icons-material/Add';
+import SidebarChannel from './SidebarChannel';
+import MicIcon from '@mui/icons-material/Mic';
+import HeadphonesIcon from '@mui/icons-material/Headphones';
+import SettingsIcon from '@mui/icons-material/Settings';
 
 const Sidebar = () => {
   return (
@@ -27,6 +31,28 @@ const Sidebar = () => {
           <h4>プログラミングチャンネル</h4>
           </div>
           <AddIcon className="sidebarAddIcon"/>
+        </div>
+
+        <div className="sidebarChannelsList">
+          <SidebarChannel />
+          <SidebarChannel />
+          <SidebarChannel />
+          <SidebarChannel />
+        </div>
+
+        <div className="sidebarFooter">
+          <div className="sidebarAccount">
+            <img src="./icon.png" alt="" />
+            <div className="accountName">
+              <h4>shincode</h4>
+              <span>#8162</span>
+            </div>
+          </div>
+          <div className="sidebarVoice">
+          <MicIcon />
+          <HeadphonesIcon />
+          <SettingsIcon />
+          </div>
         </div>
       </div>
       </div>
