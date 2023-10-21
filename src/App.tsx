c6f9b3bf496e763d800b441e739b359678e0326e
takashiono2 <takashiono710@gmail.com>
2023-10-21 18:26:01 +0900

    section4 35

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.scss';
 import Sidebar from './components/sidebar/Sidebar';
 import Chat from './components/chat/Chat';
 import './components/login/Login.scss';
 import Login from './components/login/Login';
-import { useAppSelector } from './app/hooks';
+import { useAppDispatch,useAppSelector } from './app/hooks';
+import { login,logout } from "./features/userSlice"
+import { auth } from "./firebase";
 
 //useAppSelectorとして新たに型定義し、App.tsxで使用
 function App() {
   const user = useAppSelector((state) => state.user);
+  const dispatch = useAppDispatch();
+  useEffect(()=>{
+    auth.onAuthStateChanged((loginUser)=>{
+      if(loginUser){
+        console.log(loginUser);
+        dispatch(
+          login({
+            uid: loginUser.uid,
+            photo: loginUser.photoURL,
+            email: loginUser.email,
+            displayName: loginUser.displayName,
+          })
+        );
+      }else{
+        dispatch(logout)
+      }
+    })
+  },[dispatch])
 
   return (
     <div className="App">
@@ -18,7 +38,9 @@ function App() {
           <Chat />
         </>
       ) : (
-        <Login />
+        <>
+          <Login />
+        </>
       )
     }
     </div>
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,11 @@
 //storeを生成する
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../features/userSlice";
-
+//storeを作る
 export const store = configureStore({
   reducer: userReducer,
 })
-
+//型を決める
 export type AppDispatch = typeof store.dispatch;
 
 export type RootState = ReturnType< typeof store.getState >
