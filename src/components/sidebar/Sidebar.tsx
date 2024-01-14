5e220150296328bfdf489ee45d92ad3566ace055
takashiono2 <takashiono710@gmail.com>
2024-01-14 18:12:56 +0900

    section3_15

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import "./Sidebar.scss";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import AddIcon from '@mui/icons-material/Add';
+import SidebarChannel from './SidebarChannel';
 
 const Sidebar = () => {
   return (
@@ -18,6 +21,22 @@ const Sidebar = () => {
       <div className="sidebarRight">
         <div className="sidebarTop">
           <h3>Discord</h3>
+          <ExpandMoreIcon />
+        </div>
+        <div className="sidebarChannels">
+          <div className="sidebarChannelsHeader">
+            <div className="sidebarHeader">
+              <ExpandMoreIcon />
+              <h4>プログラミングチャンネル</h4>
+            </div>
+            <AddIcon className="sidebarAddIcon" />
+          </div>
+          <div className="sidebarChannelList">
+            <SidebarChannel />
+            <SidebarChannel />
+            <SidebarChannel />
+            <SidebarChannel />
+          </div>
         </div>
       </div>
     </div>
